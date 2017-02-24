c2a408a1b0c5e0d90da11c2ec6161c52ae981d9b
Jared Winick <jaredwinick@koverse.com>
2017-02-24 07:07:09 -0700

    add new constant for count table

--- src/main/java/com/github/jaredwinick/ExerciseConstants.java
+++ src/main/java/com/github/jaredwinick/ExerciseConstants.java
@@ -6,5 +6,6 @@ public class ExerciseConstants {
 	public static final String PASSWORD = "password";
 	public static final String RECORD_TABLE = "record";
 	public static final String INDEX_TABLE = "index";
+	public static final String COUNT_TABLE = "count";
 
 }
